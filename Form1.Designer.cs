b92531a6081ccf7ae6f1951673b6e9060284963d
kreegkreng <kadyrovrenat800@gmail.com>
2025-05-29 10:48:58 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Рандомайзер
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.from1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.to1 = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.generate = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.swo = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 115);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Случайное число:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Количество чисел:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(301, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Диапазон случайных чисел:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(587, 227);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "от";
+            // 
+            // from1
+            // 
+            this.from1.AutoCompleteCustomSource.AddRange(new string[] {
+            "1"});
+            this.from1.Location = new System.Drawing.Point(627, 225);
+            this.from1.Multiline = true;
+            this.from1.Name = "from1";
+            this.from1.Size = new System.Drawing.Size(42, 37);
+            this.from1.TabIndex = 6;
+            this.from1.TextChanged += new System.EventHandler(this.from1_TextChanged);
+            this.from1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.from1_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(675, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "до";
+            // 
+            // to1
+            // 
+            this.to1.AutoCompleteCustomSource.AddRange(new string[] {
+            "9999999"});
+            this.to1.Location = new System.Drawing.Point(718, 225);
+            this.to1.Multiline = true;
+            this.to1.Name = "to1";
+            this.to1.Size = new System.Drawing.Size(42, 37);
+            this.to1.TabIndex = 8;
+            this.to1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.to1_KeyPress);
+            // 
+            // Result
+            // 
+            this.Result.Font = new System.Drawing.Font("Comic Sans MS", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Result.Location = new System.Drawing.Point(103, 181);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.ReadOnly = true;
+            this.Result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Result.Size = new System.Drawing.Size(190, 66);
+            this.Result.TabIndex = 9;
+            // 
+            // generate
+            // 
+            this.generate.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.generate.Location = new System.Drawing.Point(509, 290);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(190, 54);
+            this.generate.TabIndex = 10;
+            this.generate.Text = "Сгенерировать";
+            this.generate.UseVisualStyleBackColor = false;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(627, 146);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 35);
+            this.numericUpDown1.TabIndex = 12;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // swo
+            // 
+            this.swo.FormattingEnabled = true;
+            this.swo.Items.AddRange(new object[] {
+            "Случайное число",
+            "Несколько чисел"});
+            this.swo.Location = new System.Drawing.Point(627, 92);
+            this.swo.Name = "swo";
+            this.swo.Size = new System.Drawing.Size(180, 37);
+            this.swo.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(415, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(212, 29);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Выберите свойство:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(103, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 54);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(861, 455);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.to1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.from1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.swo);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Рандомайзер";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox from1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox to1;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox swo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Рандомайзер
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            GenerateNumbers();
+        }
+        private void GenerateNumbers()
+        {
+            Result.Clear();
+            swo.Items.Clear();
+
+            if (!int.TryParse(from1.Text, out int min))
+            {
+                MessageBox.Show("Неверное значение в поле 'from'");
+                return;
+            }
+
+            if (!int.TryParse(to1.Text, out int max))
+            {
+                MessageBox.Show("Неверное значение в поле 'to'");
+                return;
+            }
+
+            if (min > max)
+            {
+                MessageBox.Show("'from' должно быть меньше или равно 'to'");
+                return;
+            }
+
+            int count = (int)numericUpDown1.Value;
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = rnd.Next(min, max + 1);
+                numbers.Add(number);
+                swo.Items.Add(number);
+            }
+
+            // Вывод через запятую
+            Result.Text = string.Join(", ", numbers);
+
+            if (swo.Items.Count > 0)
+                swo.SelectedIndex = 0;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            GenerateNumbers();
+        }
+
+        private void from1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void from1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void to1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Result.Clear();
+        }
+    }
+}
